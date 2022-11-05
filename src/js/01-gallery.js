8da897bd8b93e4745629cb3715eaad7620c9976c
Skobel58912171 <106476498+Skobel58912171@users.noreply.github.com>
2022-11-05 14:57:43 +0300

    task 1 and 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryModal = document.querySelector('.gallery');
+// console.log(galleryModal);
+
+const markup = galleryItems
+  .map(
+    ({
+      preview,
+      original,
+      description,
+    }) => `<div><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></div>`
+  )
+  .join('');
+galleryModal.insertAdjacentHTML('afterbegin', markup);
+console.log(galleryModal);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+gallery.on('show.simplelightbox', function (evt) {});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
+const ref = {
+  form: document.querySelector('.feedback-form'),
+  inputEmail: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+// console.log(ref.form);
+// console.log(ref.textarea);
+// console.log(ref.inputEmail);
+const STORAGE_KEY = 'feedback-form-state';
 
+const user = {};
+
+ref.form.addEventListener('submit', onSubmit);
+ref.form.addEventListener('input', throttle(onInput, 500));
+populateForm();
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  console.log(user);
+  localStorage.removeItem(STORAGE_KEY);
+}
+function onInput(evt) {
+  (user.email = ref.inputEmail.value),
+    (user.message = ref.textarea.value),
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(user));
+}
+
+function populateForm() {
+  // try {
+  const saveFormFields = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (saveFormFields) {
+    ref.inputEmail.value = saveFormFields.email;
+    ref.textarea.value = saveFormFields.message;
+  }
+}
